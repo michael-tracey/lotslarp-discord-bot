136099d9fbd4a465ee82d91bd3b89c9d39a1a5e4
mogmismo <mogmismo@gmail.com>
2025-04-11 13:00:15 +0000

    feat(bot): initial

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+try:
+    client.run(os.environ["DISCORD_BOT_TOKEN"])
+except KeyError:
+    print("Please set the environment variable DISCORD_BOT_TOKEN with your bot token.")
